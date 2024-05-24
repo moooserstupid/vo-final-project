d4d6b7f3cc52242a09d88324e0ba62988aad7514
Ali Asghar <aldigod123@gmail.com>
2024-05-24 06:44:20 +0300

    First iteration that worked well with custom data

---
+++ run_custom.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Launching Custom Dataset with Stereo sensor"
+./final_vo ./Vocabulary/ORBvoc.txt ./Settings/Custom_Ali.yaml /home/ali/Work/Programming/Cpp/calibration/dataset-dir /home/ali/Work/Programming/Cpp/calibration/dataset-dir/custom_timestamps.txt
